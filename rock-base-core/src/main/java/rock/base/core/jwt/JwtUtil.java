bb23396bee92f1c20580cfdd5e313d9c87eef40a
i544693 <koy@ko8e24.top>
2022-02-08 23:39:32 +0800

    add(core):jwt.

---
+++ rock-base-core/src/main/java/rock/base/core/jwt/JwtUtil.java
@@ -0,0 +1,49 @@
+package rock.base.core.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtil {
+
+    private static final long DEFAULT_TOKEN_EXPIRED_TIME = 30 * 24 * 60 * 60;
+
+    private static final String JWT_SECRET = "ThisIsRockBootJwtSecretKey1234567890";
+    private static final SecretKey secretKey = Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+
+    public static String createJWT(Map<String, Object> claims) {
+
+
+        final long currentTimeMillis = System.currentTimeMillis();
+        Date now = new Date(currentTimeMillis);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .setExpiration(new Date(currentTimeMillis + DEFAULT_TOKEN_EXPIRED_TIME))
+                .compact();
+    }
+
+    public static Claims verifyJwt(String token) {
+
+        Claims claims = null;
+        try {
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+        } catch (Exception ignore) {
+        }
+        return claims;
+
+    }
+
+}
---
+++ rock-base-core/src/test/java/rock/base/core/jwt/JwtUtilTest.java
@@ -0,0 +1,30 @@
+package rock.base.core.jwt;
+
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtilTest {
+
+    private String token;
+
+    @BeforeEach
+    void ShouldCreateJwt_GivenClaims_WhenCallCreateJwt() {
+        final Map<String, Object> map = new HashMap<>();
+        map.put("user", "rock");
+        map.put("sub", "root");
+        token = JwtUtil.createJWT(map);
+        Assertions.assertTrue(StringUtils.hasLength(token));
+    }
+
+    @Test
+    void ShouldVerifyJwt_GivenToken_WhenCallVerifyJwt() {
+        final Claims claims = JwtUtil.verifyJwt(token);
+        Assertions.assertEquals("root", claims.getSubject());
+    }
+}
